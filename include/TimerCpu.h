a96a1b3422828aee9a65c8cc515183d7c0476322
duhamel <elie.duhamel@grenoble-inp.org>
2026-01-20 12:08:14 +0100

    TimeCpu measure time ok

--- include/TimerCpu.h
+++ include/TimerCpu.h
@@ -4,19 +4,35 @@
 
 #ifndef BENCHMARK_LINUX_TIMERCPU_H
 #define BENCHMARK_LINUX_TIMERCPU_H
-#include <cstdint>
-typedef std::uint64_t time_cpu_t ;
-typedef  int time_real_t;
+#include <chrono>
+#include <bits/posix2_lim.h>
 
+typedef timespec time_cpu_t ;
+typedef std::chrono::time_point<std::chrono::high_resolution_clock> time_real_t;
+struct Duration_Timer_Cpu;
 class TimerCpu
 {
     int num_cpu_;
+    time_cpu_t start_time_;
+    time_cpu_t stop_time_;
+    time_real_t start_time_real_;
+    time_real_t stop_time_real_;
     public:
     TimerCpu();
-    time_cpu_t get_time_cpu();
+    void start();
+    void stop();
+    Duration_Timer_Cpu dure();
 
 
-    ~TimerCpu();
+
+    ~TimerCpu()=default;
+};
+
+struct Duration_Timer_Cpu
+{
+    long long int cpu_duration;
+    long long int real_duration;
+
 };
 
 #endif //BENCHMARK_LINUX_TIMERCPU_H
--- src/TimerCpu.cpp
+++ src/TimerCpu.cpp
@@ -3,11 +3,36 @@
 //
 #include "TimerCpu.h"
 
-#include <sched.h>
-#include <bits/cpu-set.h>
+#include <chrono>
+
 
 TimerCpu::TimerCpu()
+{   //TODO initail num_cpu
+    start_time_ = {0, 0};
+    stop_time_ = {0, 0};
+    start_time_real_ = std::chrono::time_point<std::chrono::high_resolution_clock>();
+    stop_time_real_ = std::chrono::time_point<std::chrono::high_resolution_clock>();
+}
+
+void TimerCpu::start()
 {
+    start_time_real_=std::chrono::high_resolution_clock::now();
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &start_time_);
+}
+void TimerCpu::stop()
+{
+    clock_gettime(CLOCK_THREAD_CPUTIME_ID, &stop_time_);
+    stop_time_real_=std::chrono::high_resolution_clock::now();
+}
+Duration_Timer_Cpu TimerCpu::dure()
+{
+    // Calculate the duration of the timer in CPU time.
+    long long int cpu_duration = (stop_time_.tv_sec - start_time_.tv_sec) * 1000000 + (stop_time_.tv_nsec - start_time_.tv_nsec) / 1000;
 
+    // Calculate the duration of the timer in real time.
+    long long int real_duration = (stop_time_real_.time_since_epoch().count() - start_time_real_.time_since_epoch().count()) * 1000000 / std::chrono::high_resolution_clock::period::den;
 
+    // Return the duration as a struct.
+    Duration_Timer_Cpu duration = {cpu_duration, real_duration};
+    return duration;
 }
