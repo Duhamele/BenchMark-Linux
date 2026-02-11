302560a15f6c33a33061d63a77ae5d2639c718c8
duhamel <elie.duhamel@grenoble-inp.org>
2026-02-11 15:36:21 +0100

    Add Histogram method

--- include/MeasureTimer.h
+++ include/MeasureTimer.h
@@ -5,19 +5,22 @@
 #ifndef BENCHMARK_LINUX_MEASURETIMER_H
 #define BENCHMARK_LINUX_MEASURETIMER_H
 #include <vector>
+#include "Histogram.h"
 
 class MeasureTimerBase
 {
 protected:
     std::vector<long long int> values;
-    bool sort = false;
+    bool _sort = false;
 
 public:
+    void sort();
     long double average();
     long long int min();
     long long int max();
     long long int median();
     long double variance();
+    Histogram histogram();
 };
 
 
--- src/MeasureTimer.cpp
+++ src/MeasureTimer.cpp
@@ -7,6 +7,14 @@
 #include <limits.h>
 
 
+void MeasureTimerBase::sort()
+{
+    if (!_sort)
+    {
+        std::ranges::sort(values);
+    }
+}
+
 long double MeasureTimerBase::average()
 {
     long double sum=0.0;
@@ -21,7 +29,7 @@ long long int MeasureTimerBase::min()
     {
         return -1;
     }
-    else if (sort)
+    else if (_sort)
     {
         return values[0];
     }
@@ -46,10 +54,7 @@ long long int MeasureTimerBase::median()
     {
         return -1;
     }
-    if (!sort)
-    {
-        std::sort(values.begin(),values.end());
-    }
+    sort();
     if (values.size()%2==0)
     {
         return (values[values.size()/2]+values[(values.size()/2)+1])/2;
@@ -78,12 +83,39 @@ long double MeasureTimerBase::variance()
     }
 }
 
+Histogram MeasureTimerBase::histogram()
+{
+    sort();
+    double lenght=max()-min();
+    double star= std::min(min()-0.005*lenght,0.);
+    double end=max()+0.005*lenght;
+    std::vector<int> hist;
+    hist.resize(10);
+    long long int step=(end-star)/10 +star;
+    int count=0;
+    for (auto val:values)
+    {
+        if (val> step)
+        {
+            count++;
+            step+=(end-star)/10;
+        }
+        hist[count]++;
+    }
+    std::vector<double> vals(hist.begin(),hist.end());
+    return Histogram(star,end,vals);
+
+
+
+
+}
+
 long long int MeasureTimerBase::max()
 {
     if (values.empty())
     {
         return -1;
-    }else if (sort)
+    }else if (_sort)
     {
         return values.back();
     }
